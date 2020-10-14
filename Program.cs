27ba05c9d3e6bb0515391115fbf050c6535430ff
Valerian21 <72431080+Valerian21@users.noreply.github.com>
2020-10-14 22:28:46 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Результат: {x}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Возникло исключение FormatException");
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+        }
+    }
+}
